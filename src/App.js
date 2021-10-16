2558979b9cb5394586162bc04454a942c2f71861
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-10-16 17:27:09 +0330

    Completed until 03-018

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { id: 1, title: "Car insurance", date: new Date(2021, 8, 16), amount: 450 },
+    { id: 1, title: "Toilet paper", date: new Date(2021, 8, 12), amount: 200 },
+    { id: 1, title: "New House", date: new Date(2021, 7, 16), amount: 2500 },
+    { id: 1, title: "New Car", date: new Date(2021, 8, 15), amount: 8500 },
+    { id: 1, title: "Other", date: new Date(2021, 8, 19), amount: 2650 },
+  ];
+
   return (
     <div className="container">
       <div className="row">
@@ -9,7 +17,7 @@ function App() {
           <div className="mt-4">
             <h4>Main index</h4>
             <div className="mt-4">
-              <Expenses />
+              <Expenses items={expenses} />
             </div>
           </div>
         </div>
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,25 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 
 
-function Expenses() {
-    const expenses = [
-        { id: 1, title: "Car insurance", date: new Date(2021, 8, 16), amount: 450 },
-        { id: 1, title: "Toilet paper", date: new Date(2021, 8, 12), amount: 200 },
-        { id: 1, title: "New House", date: new Date(2021, 7, 16), amount: 2500 },
-        { id: 1, title: "New Car", date: new Date(2021, 8, 15), amount: 8500 },
-        { id: 1, title: "Other", date: new Date(2021, 8, 19), amount: 2650 },
-    ];
-
+function Expenses(props) {
+    const items = props.items;
     return (
         <div>
             <ExpenseItem
-                title={expenses[0].title}
-                date={expenses[0].date}
-                amount={expenses[0].amount} />
+                title={items[0].title}
+                date={items[0].date}
+                amount={items[0].amount} />
             <ExpenseItem
-                title={expenses[1].title}
-                date={expenses[1].date}
-                amount={expenses[1].amount} />
+                title={items[1].title}
+                date={items[1].date}
+                amount={items[1].amount} />
         </div>
     );
 }
