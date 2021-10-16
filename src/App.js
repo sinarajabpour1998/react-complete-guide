32bffedbe7c31ab135f48da0a8360520920f7481
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-10-16 18:23:56 +0330

    Completed until 04-02

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expenses = [
     { id: 1, title: "Car insurance", date: new Date(2021, 8, 16), amount: 450 },
-    { id: 1, title: "Toilet paper", date: new Date(2021, 8, 12), amount: 200 },
-    { id: 1, title: "New House", date: new Date(2021, 7, 16), amount: 2500 },
-    { id: 1, title: "New Car", date: new Date(2021, 8, 15), amount: 8500 },
-    { id: 1, title: "Other", date: new Date(2021, 8, 19), amount: 2650 },
+    { id: 2, title: "Toilet paper", date: new Date(2021, 8, 12), amount: 200 },
+    { id: 3, title: "New House", date: new Date(2021, 7, 16), amount: 2500 },
+    { id: 4, title: "New Car", date: new Date(2021, 8, 15), amount: 8500 },
+    { id: 5, title: "Other", date: new Date(2021, 8, 19), amount: 2650 },
   ];
 
   return (
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,23 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+    const expenseAction = () => {
+        alert(props.id + " clicked")
+    }
 
     return (
         <Card parentClasses={"container"} childClasses={"row text-center item mb-2"}>
             <ExpenseDate date={props.date} />
-            <div className="col-md-6">
+            <div className="col-md-3">
                 <p>{props.title}</p>
             </div>
             <div className="col-md-3">
                 <p>$ {props.amount}</p>
+            </div>
+            <div className="col-md-3">
+                <button onClick={expenseAction}>
+                    Edit title
+                </button>
             </div>
         </Card>
     );
