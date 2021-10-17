77d5f3089a666d178f067846942381a0f296d366
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-10-17 15:37:13 +0330

    Until 4-9, added some listeners for ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,23 @@
 import {Form} from "react-bootstrap";
 import { Button } from 'react-bootstrap';
+import {useState} from "react";
 
 const ExpenseForm = (props) => {
 
+    const [expenseTitle, expenseSetTitle] = useState('');
+    const [expenseAmount, expenseSetAmount] = useState('');
+    const [expenseDate, expenseSetDate] = useState('');
+
     const onTitleChangeEvent = (event) => {
-      console.log(event.target.value)
+        expenseSetTitle(event.target.value);
+    }
+
+    const onAmountChangeEvent = (event) => {
+      expenseSetAmount(event.target.value);
+    }
+
+    const onDateChangeEvent = (event) => {
+      expenseSetDate(event.target.value);
     }
 
     return (
@@ -20,13 +33,13 @@ const ExpenseForm = (props) => {
                   <div className="col-md-4">
                       <Form.Group className="mb-3" controlId="amount">
                           <Form.Label>Amount</Form.Label>
-                          <Form.Control type="number" min="0.01" step="0.01" />
+                          <Form.Control type="number" min="0.01" step="0.01" onChange={onAmountChangeEvent} />
                       </Form.Group>
                   </div>
                   <div className="col-md-4">
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label>Date</Form.Label>
-                          <Form.Control type="date" min="2021-1-1" max="2022-12-28" />
+                          <Form.Control type="date" min="2021-1-1" max="2022-12-28" onChange={onDateChangeEvent} />
                       </Form.Group>
                   </div>
               </div>
