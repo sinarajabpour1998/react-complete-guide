4d3de81a7ca5873ed192040146bcfa9717fc76ac
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-10-21 23:44:40 +0330

    Until 4-13, Adding Two-Way Binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,6 +30,10 @@ const ExpenseForm = (props) => {
       }
 
       console.log(expenseData);
+
+      expenseSetTitle('');
+      expenseSetAmount('');
+      expenseSetDate('');
     }
 
     return (
@@ -39,19 +43,19 @@ const ExpenseForm = (props) => {
                   <div className="col-md-4">
                       <Form.Group className="mb-3" controlId="title">
                           <Form.Label>Title</Form.Label>
-                          <Form.Control type="text" onChange={onTitleChangeEvent} />
+                          <Form.Control type="text" value={expenseTitle} onChange={onTitleChangeEvent} />
                       </Form.Group>
                   </div>
                   <div className="col-md-4">
                       <Form.Group className="mb-3" controlId="amount">
                           <Form.Label>Amount</Form.Label>
-                          <Form.Control type="number" min="0.01" step="0.01" onChange={onAmountChangeEvent} />
+                          <Form.Control type="number" min="0.01" step="0.01" value={expenseAmount} onChange={onAmountChangeEvent} />
                       </Form.Group>
                   </div>
                   <div className="col-md-4">
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label>Date</Form.Label>
-                          <Form.Control type="date" min="2021-1-1" max="2022-12-28" onChange={onDateChangeEvent} />
+                          <Form.Control type="date" min="2021-1-1" max="2022-12-28" value={expenseDate} onChange={onDateChangeEvent} />
                       </Form.Group>
                   </div>
               </div>
