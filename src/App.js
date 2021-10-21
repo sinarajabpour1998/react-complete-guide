7f0cbaca52655e7582c2bd993341db58f3153134
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-10-22 01:12:25 +0330

    Until 5-3, Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.scss';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import {useState} from "react";
+
+const DUMMY_EXPENSES = [
+  { id: 1, title: "Car insurance", date: new Date(2021, 8, 16), amount: 450 },
+  { id: 2, title: "Toilet paper", date: new Date(2021, 8, 12), amount: 200 },
+  { id: 3, title: "New House", date: new Date(2021, 7, 16), amount: 2500 },
+  { id: 4, title: "New Car", date: new Date(2021, 8, 15), amount: 8500 },
+  { id: 5, title: "Other", date: new Date(2021, 8, 19), amount: 2650 },
+];
 
 function App() {
-  const expenses = [
-    { id: 1, title: "Car insurance", date: new Date(2021, 8, 16), amount: 450 },
-    { id: 2, title: "Toilet paper", date: new Date(2021, 8, 12), amount: 200 },
-    { id: 3, title: "New House", date: new Date(2021, 7, 16), amount: 2500 },
-    { id: 4, title: "New Car", date: new Date(2021, 8, 15), amount: 8500 },
-    { id: 5, title: "Other", date: new Date(2021, 8, 19), amount: 2650 },
-  ];
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const handleExpenseNewData = (expenseData) => {
-    console.log("App js call");
-    console.log(expenseData);
+    setExpenses(prevState => {
+      return [expenseData, ...prevState]
+    });
   }
 
   return (
